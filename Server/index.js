4e43010cd65dc22534bf35ed40abe117512c1405
Rushikesh-wagh <111699859+Rishi-Wagh@users.noreply.github.com>
2024-01-22 00:46:36 +0530

    Update index.js

--- Server/index.js
+++ Server/index.js
@@ -7,17 +7,26 @@ import listingRouter from './routes/Auth.route.js';
 import allListingRouter from './routes/Auth.route.js';
 import {errorHandler} from "./utils/error.js";
 import cookieParser from "cookie-parser";
+import path from "path";
 
 dotenv.config();
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
 
+//dynamic dir-name
+const __dirname = path.resolve();
+
 //Routes
 app.use('/api/auth', authRouter);
 app.use('/api/user', userRouter);
 app.use('/api/listing', listingRouter);
-app.use('/api/home', allListingRouter )
+app.use('/api/home', allListingRouter );
+
+app.use(express.static(path.join(__dirname, '/client/dist')));
+app.get('*', (req,res)=>{
+  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'))
+})
 
 //middlewear
 app.use(errorHandler);
@@ -30,6 +39,7 @@ app.use(errorHandler);
    .catch((err) => {
     console.log(err);
    })
+
 
 //Connection to port   
 app.listen(3000 , ()=>{
